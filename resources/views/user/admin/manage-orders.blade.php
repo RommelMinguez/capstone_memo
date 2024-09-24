8cb32320a0a4b3119e14d41fe8037034f77ac7a8
rml <rommeluminguez@gmail.com>
2024-09-24 11:59:45 +0800

    manage order static

--- resources/views/user/admin/manage-orders.blade.php
+++ resources/views/user/admin/manage-orders.blade.php
@@ -18,35 +18,41 @@
 
                     <div class="p-5">
 
-                        <div class="font-bold text-xl">
-                            Orders
-                        </div>
-                        <br><br>
-
                         <div class="flex justify-between">
-                            <ul class="flex cursor-pointer border-b-2 w-fit relative text-sm">
-                                <li class="order-tab px-5 py-2 font-semibold hover:text-[#F55447] z-20">All Orders</li>
-                                <li class="order-tab px-5 py-2 font-semibold hover:text-[#F55447] z-20">Pending</li>
-                                <li class="order-tab px-5 py-2 font-semibold hover:text-[#F55447] z-20">Baking</li>
-                                <li class="order-tab px-5 py-2 font-semibold hover:text-[#F55447] z-20">To Receive</li>
-                                <li class="order-tab px-5 py-2 font-semibold hover:text-[#F55447] z-20 ">To Review</li>
-                                <li class="order-tab px-5 py-2 font-semibold hover:text-[#F55447] z-20">Completed</li>
-                                <li class="order-tab px-5 py-2 font-semibold hover:text-[#F55447] z-20">Canceled</li>
-                            </ul>
-
-                            <div>
-                                <input type="text" class="w-60 px-2 py-1" placeholder="Search Here...">
+                            <div class="font-bold text-xl">
+                                Orders
+                            </div>
+                            <div class="flex gap-2">
+                                <input type="text" class="w-80 p-2 rounded-md border shadow-md" placeholder="Search Here...">
+                                <button type="button" class="p-2 rounded-md bg-[#F55447] text-white shadow-md font-semibold hover:bg-red-500 active:scale-95">+ Create Order</button>
                             </div>
                         </div>
 
+                        <br><br>
+
+                        <div class="flex">
+                            <ul class="flex cursor-pointer w-fit relative text-sm">
+                                <li class="order-tab px-5 pt-2 pb-4 font-semibold hover:text-[#F55447] z-20 border-b-2 border-red-500 bg-[#eedee4] rounded-t-lg text-red-500">All Orders</li>
+                                <li class="order-tab px-5 pt-2 pb-4 font-semibold hover:text-[#F55447] z-20">Pending</li>
+                                <li class="order-tab px-5 pt-2 pb-4 font-semibold hover:text-[#F55447] z-20">Baking</li>
+                                <li class="order-tab px-5 pt-2 pb-4 font-semibold hover:text-[#F55447] z-20">To Receive</li>
+                                <li class="order-tab px-5 pt-2 pb-4 font-semibold hover:text-[#F55447] z-20 ">To Review</li>
+                                <li class="order-tab px-5 pt-2 pb-4 font-semibold hover:text-[#F55447] z-20">Completed</li>
+                                <li class="order-tab px-5 pt-2 pb-4 font-semibold hover:text-[#F55447] z-20">Canceled</li>
+                            </ul>
+                        </div>
+
                         <table class="border-collapse w-full table-fixed">
-                            <thead class="border-y-2">
+                            <thead class="border-b-2">
                                 <tr class="bg-[#eedee4]">
-                                    <th class="p-2 w-20">No.</th>
+                                    <th class="p-2 w-20">ID</th>
                                     <th class="p-2 w-auto">Customer Name</th>
-                                    <th class="p-2 w-auto">Cake</th>
-                                    <th class="p-2 w-auto">Date</th>
+                                    <th class="p-2 w-auto">Cake Name</th>
+                                    <th class="p-2 w-auto">Order Date</th>
+                                    <th class="p-2 w-auto">Total Amount</th>
                                     <th class="p-2 w-40">Status</th>
+                                    <th class="p-2 w-auto">Delivery Date</th>
+                                    <th class="p-2 w-20">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -56,7 +62,14 @@
                                         <td >Juan Dela Cruz</td>
                                         <td >Vanilla Cake</td>
                                         <td >2024/09/24</td>
+                                        <td >150.00</td>
                                         <td >pending</td>
+                                        <td >2024/09/24</td>
+                                        <td class="py-2">
+                                            <select class="text-white text-xs p-1 rounded-sm bg-[#F55447]">
+                                                <option value="pending">Pending</option>
+                                            </select>
+                                        </td>
                                     </tr>
                                 @endfor
                             </tbody>
@@ -76,9 +89,9 @@
         tabs.forEach((element, index) => {
             element.addEventListener('click', function() {
                 tabs.forEach((e, i) => {
-                    e.classList.remove('border-b-2', 'border-red-500',  'bg-[#eaeaea]', 'rounded-t-lg', 'text-red-500');
+                    e.classList.remove('border-b-2', 'border-red-500',  'bg-[#eedee4]', 'rounded-t-lg', 'text-red-500');
                 });
-                element.classList.add('border-b-2', 'border-red-500',  'bg-[#eaeaea]', 'rounded-t-lg', 'text-red-500');
+                element.classList.add('border-b-2', 'border-red-500',  'bg-[#eedee4]', 'rounded-t-lg', 'text-red-500');
             });
         });
     </script>
